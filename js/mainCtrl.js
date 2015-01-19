a28c19f0a6334fce3692657f581ce50d000fbf67
Fredrick Jones <fredrick.w.jones@gmail.com>
2015-01-19 14:16:52 -0700

    finished

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -5,7 +5,13 @@ var app = angular.module('friendsList');
 
 app.controller('mainCtrl', function($scope) {
 	$scope.friends = ['Ben Eliason', 'Mike Every', 'Mike Kitto', 'Shevin Morris', 'Adam Hullinger'];
-	var addFriends = function() {
-		adlkj
+	// var addFriends = function() {
+	// 	adlkj
+	// };
+
+	$scope.showFriends = true;
+
+	$scope.toggleFriends = function() {
+		$scope.showFriends = !$scope.showFriends;
 	};
 });
